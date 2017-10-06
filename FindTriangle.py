72d7b5a939e080ba501959a8415d142836fcff13
Derrick Wong <dcw008@ucsd.edu>
2017-10-05 17:08:09 -0700

    first commit

---
+++ FindTriangle.py
@@ -0,0 +1,54 @@
+
+#Given a graph G in the form of a adjacency matrix, determine if the Graph contains a triangle.
+
+
+testMatrix = [[0,1,0,0,1], [1,0,1,0,1], [0,1,0,1,0], [0,0,1,0,1], [0,1,0,1,0]]
+
+#This algorithm uses a bucketing approach such that we will process the entire matrix and add edges into buckets.
+#We only add buckets to our bucket list if a bucket has at least 2 edges because in order to form a triangle, a bucket
+#must have at least two edges in the form (u, v) (u, w). After processing the all the edges from the matrix,
+#we iterate through each bucket and for each pair of edges we check if there an edge exists (v,w) by checking our matrix
+#If there is, return true. Else, return false
+def findTriangle(G):
+
+    #a list of all the buckets
+    allBuckets = []
+    #set of edges (u, v) so that if we see edge (v, u) we don't need to process that edge
+    symmetricSet = set()
+
+    #iterate through each row
+    for i, row in enumerate(G):
+        #form the bucket for each row
+        bucket = []
+        #check for edges in each column
+        for j, col in enumerate(G):
+            #ignore the edge if we have seen it
+            if((j, i) in symmetricSet):
+                continue
+            else:
+                if(G[i][j] == 1):
+                    #append the edge (u,v) to the bucket list
+                    bucket.append((i, j))
+                    #add the edge to the set
+                    symmetricSet.add((i,j))
+
+        #iterate through the bucket to find a triangle
+        if(len(bucket) >= 2):
+            allBuckets.append(bucket)
+
+    #process the buckets to find the triangles
+    for bucket in allBuckets:
+        # print(bucket)
+        #for given nodes (u,v) and (u,w) try to find (v,w)
+        it = iter(bucket)
+        for edge in it:
+            firstEdge = edge
+            secondEdge = next(it)
+            u = firstEdge[1]
+            v = secondEdge[1]
+            if(G[u][v] == 1):
+                return True
+    return False
+
+print(findTriangle(testMatrix))
+
