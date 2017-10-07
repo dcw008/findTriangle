cdb40dfd237d2b0b61f9c665c09521b2b17f4ced
Rajit Dang <r3dang@ucsd.edu>
2017-10-06 19:05:56 -0700

    fixed bug

--- RandomMatrix.py
+++ RandomMatrix.py
@@ -5,14 +5,24 @@ import FindTriangle
 def createRandomMatrix(n):
 	# create a matrix and set all values of the matrix to 0
 	randomMatrix = [[0 for x in range(n)] for y in range(n)]
+	symetricSet = set()
 	for i, row in enumerate(randomMatrix):
 		for j, col in enumerate(randomMatrix):
 			matrixRand = random.random()
-
-			if matrixRand >= 0.5:
-				randomMatrix[i][j] = 1
-			else:
+			if j == i:
 				randomMatrix[i][j] = 0
+			if((j, i) in symmetricSet):
+                continue
+			else:
+				if matrixRand >= 0.5:
+					randomMatrix[i][j] = 1
+					randomMatrix[j][i] = 1
+					symmetricSet.add((i,j))
+				else:
+					randomMatrix[i][j] = 0
+					randomMatrix[j][i] = 0
+					symmetricSet.add((i,j))
+
 	return randomMatrix
 
 def printMatrix(G):
