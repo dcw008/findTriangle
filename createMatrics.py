f1fcf9301b7e3450c47842e874aa0264fb174075
Rajit Dang <r3dang@ucsd.edu>
2017-10-06 17:36:01 -0700

    Added gitignore and createRandom

---
+++ createMatrics.py
@@ -0,0 +1,20 @@
+import random
+
+def createRandomMatrix(n):
+	# create a matrix and set all values of the matrix to 0
+	randomMatrix = [[0 for x in range(n)] for y in range(n)]
+	for i, row in enumerate(randomMatrix):
+		for j, col in enumerate(randomMatrix):
+			matrixRand = random.random()
+
+			if matrixRand >= 0.5:
+				randomMatrix[i][j] = 1
+			else:
+				randomMatrix[i][j] = 0
+	return randomMatrix
+
+def printMatrix(G):
+	print('\n'.join([''.join(['{:4}'.format(item) for item in row]) for row in G]))
+
+
+printMatrix(createRandomMatrix(4))
