00e13fbf9b9cf6340ba6f4309c2497ebd713a5eb
Rajit Dang <r3dang@ucsd.edu>
2017-10-07 14:52:57 -0700

    didnt change much, alg needs fix imo

--- testGraph.py
+++ testGraph.py
@@ -3,7 +3,7 @@ import time
 import RandomMatrix
 
 # this file is for testing and running findTriangles on randomly generated undirected graph
-size_vector = [4, 8, 16, 32, 64, 128, 256, 512, 1024]
+size_vector = [8]
 time_vector = []
 for j in range(0, len(size_vector)):
 	G = RandomMatrix.createRandomMatrix(size_vector[j])
@@ -11,11 +11,11 @@ for j in range(0, len(size_vector)):
 	print('\n')
 	
 	start = time.time()
-	for i in range(0, 200):
+	for i in range(0, 2):
 	    FindTriangle.findTriangle(G)
 	end = time.time()
 
-	run_time = (end - start)/200
+	run_time = (end - start)/2
 	time_vector.append(run_time)
 print('\n\n')
 for k in range(0, len(time_vector)):
